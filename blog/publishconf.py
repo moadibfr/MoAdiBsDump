f614a2a7b1b896432bf408ea8018e429bf9eb79b
Martin GUIBERT <martin@keecker.com>
2014-06-19 12:54:48 +0200

    initial commit

---
+++ blog/publishconf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'MoAdiB'
+SITENAME = u"MoAdiB's Dump"
+SITESUBTITLE = u"MoAdiB's Dump"
+SITEURL = 'http://blog.moadib.net'
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+GITHUB_URL = None
+DISQUS_SITENAME = None
+# Feed generation is usually not desired when developing
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+
+PDF_GENERATOR = False
+
+DEFAULT_METADATA = (('description', 'MoAdiB\'s Mess, a place where I dump things about geek things, tech, android, developpement and feminism...'),)
+
+DEFAULT_PAGINATION = 10
+
+THEME = "themes/soma"
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = False
